3eef5549cdbd6faac0ca6d891bcaeffbb3a39cb1
Thomas-Alexandre Moreau <thomas-alexandre.moreau@etu.u-bordeaux.fr>
2023-01-03 01:00:36 +0100

    03/01/23

---
+++ Fac/L2/Premier semestre/Réseau/TP 8/myscript.js
@@ -0,0 +1,9 @@
+function func1() {
+    let elm = document.querySelector("h1");
+    elm.textContent = "LES MMORPG aka Massively Multiplayer Online Role-Playing Game";
+}
+
+function func2() {
+    let elm = document.getElementById("imgwow");
+    elm.
+}
