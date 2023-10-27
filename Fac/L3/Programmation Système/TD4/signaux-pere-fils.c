9704d45e4f2ebd2bfe78580b57821c2e664a9930
Thomas-Alexandre Moreau <thomas-alexandre.moreau@etu.u-bordeaux.fr>
2023-10-27 12:22:57 +0200

    27/10/23

--- Fac/L3/Programmation Système/TD4/signaux-pere-fils.c
+++ Fac/L3/Programmation Système/TD4/signaux-pere-fils.c
@@ -10,42 +10,60 @@
 
 #define NSIGNORT 32
 
-int emetteur(int pere, int argc, char * argv[]) {
+struct sigaction old;
+int acc = 1;
+
+void handler(int sig)
+{
+  printf("%d - %s\n", acc, strsignal(sig));
+  acc++;
+}
+
+int emetteur(int pere, int argc, char *argv[])
+{
   int k = atoi(argv[1]);
 
-  sleep(1); 
+  sleep(1);
 
-  for(int i = 0 ; i < k ; i++) 
-    for(int j = 2; j < argc; j++){
-      kill(pere,atoi(argv[j]));
-  }
+  for (int i = 0; i < k; i++)
+    for (int j = 2; j < argc; j++)
+    {
+      kill(pere, atoi(argv[j]));
+      sleep(1);
+    }
 
-  //kill(pere,9);
+  sleep(1);
+  kill(pere, 9);
   return 0;
 }
 
-
-
-int recepteur(int fils) {
+int recepteur(int fils)
+{
   printf("récepteur : %d\n", getpid());
 
-  // installation du handler pour tous les signaux non RT  
+  // installation du handler pour tous les signaux non RT
 
-  for(int sig = 0 ; sig < NSIGNORT ; sig++) {
-    //sigaction( , , ); 
+  struct sigaction sa;
+  sa.sa_flags = 0;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_handler = handler;
+
+  for (int sig = 0; sig < NSIGNORT; sig++)
+  {
+    sigaction(sig, &sa, NULL);
   }
-    
-  while(1) 
+
+  while (1)
     pause();
-  
+
   return 0;
 }
 
-
-int main(int argc, char *argv[]){
-  pid_t pid = fork();  
+int main(int argc, char *argv[])
+{
+  pid_t pid = fork();
   if (pid == 0)
-    emetteur(getppid(),argc,argv);
+    emetteur(getppid(), argc, argv);
   else
-    recepteur(pid);  
+    recepteur(pid);
 }
